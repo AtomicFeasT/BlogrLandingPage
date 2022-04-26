9808f4779277eabe4d4ca22a863aee1fac449823
AtomicFeasT <atomic-feast@hotmail.com>
2022-04-26 14:41:46 -0300

    fix(astro.config.mjs): fixed root path

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
-    root: '/blogr-landing-page',
+    root: '/',
     publicDir: 'public',
     site: 'https://atomicfeast.github.io/blogr-landing-page/'
 });
