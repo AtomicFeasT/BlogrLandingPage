0b5249669f9ff810ec1fb41fa598583bafa8ca59
AtomicFeasT <atomic-feast@hotmail.com>
2022-04-26 14:11:57 -0300

    feat(astro.config.mjs): added a site option to the file

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,5 @@ import { defineConfig } from 'astro/config';
 export default defineConfig({
     root: "/",
     publicDir: 'public',
-    
+    site: 'https://atomicfeast.github.io/blogr-landing-page/'
 });
