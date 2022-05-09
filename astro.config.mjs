68682d3d3f60bf0ce0b4b3d8825a4749701b8e65
AtomicFeasT <atomic-feast@hotmail.com>
2022-05-09 16:10:28 -0300

    Vite migration

--- astro.config.mjs
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'astro/config';
-// https://astro.build/config
-/** @type {import('astro').AstroUserConfig} */
-export default  defineConfig({
-    root: "./",
-    publicDir: "./public",
-    base: '/blogr-landing-page'
-});
